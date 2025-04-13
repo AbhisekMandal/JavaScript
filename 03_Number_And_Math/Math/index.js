4872c12a24c0c88e1f84d4691c42a5ec8bdc8478
Abhisek <mandalabhisek20@gmail.com>
2025-04-13 16:27:07 +0530

    Math Object and Methods Added

---
+++ 03_Number_And_Math/Math/index.js
@@ -0,0 +1,25 @@
+console.log(Math);
+
+const num1=121;
+const num2=36.635;
+
+// console.log(Math.abs(num2));
+// console.log(Math.round(num2));
+// console.log(Math.ceil(num2));
+// console.log(Math.floor(num2));
+// console.log(Math.max(2,5,4,7,6,8));
+// console.log(Math.min(2,5,4,7,6,8));
+
+console.log(Math.random()); //0-1
+
+
+const min=1;
+const max=6;
+console.log (Math.floor((Math.random() * (max-min + 1)+min)));
+
+
+
+
+
+
+
