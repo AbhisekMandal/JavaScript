0d9f8a8c328e9cc5983721e4c5498023d989b116
Abhisek <mandalabhisek20@gmail.com>
2025-04-13 16:37:38 +0530

    Date Object Added

---
+++ 04_Date_and_Time/index.js
@@ -0,0 +1,15 @@
+// Date 
+// Starting from 01 JAN 1970
+console.log(Date());
+
+
+const date=new Date();
+console.log(typeof date);
+
+console.log(date.toDateString());
+console.log(date.toString());
+console.log(date.toJSON());
+console.log(date.toLocaleString());
+
+let mydate =new Date(2023,0,23)
+console.log(mydate.toDateString());
